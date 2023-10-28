b00ee7b07fba202f33c666fafc4686556621bc94
c0dedance <2627702283@qq.com>
2023-10-28 15:27:43 +0800

    feat: init

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import monkey from 'vite-plugin-monkey';
+
+export default defineConfig({
+  plugins: [
+    monkey({
+      entry: 'src/main.js',
+      userscript: {
+        icon: 'https://vitejs.dev/logo.svg',
+        match: ['https://juejin.cn/book/*',
+          'https://juejin.cn/video/*'],
+        author: 'c0dedance',
+        "run-at": "document-start",
+        name: "juejin-book-exporter",
+        namespace: "http://tampermonkey.net/",
+        version: "0.0.1"
+      },
+    }),
+  ],
+  build: {
+    minify: true
+    // rollupOptions: {
+    //   output: {
+    //     format: "iife"
+    //   }
+    // }
+  }
+});
